0bfd901f3ed1b82ba0552d7b47282154b99b1feb
Q Z <13253467705@163.com>
2023-10-18 15:34:42 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FoundationEx",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FoundationEx",
+            targets: ["FoundationEx"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FoundationEx",
+            dependencies: [
+              "RxSwift"
+            ]),
+        .testTarget(
+            name: "FoundationExTests",
+            dependencies: ["FoundationEx"]),
+    ]
+)
